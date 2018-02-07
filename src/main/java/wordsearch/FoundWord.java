ab1bda944a6b034a449a5c846aa8fd185a7a4e68
mwzig <mwziegler2014@gmail.com>
2018-02-07 11:13:31 -0500

    Add FoundWord class and TestFoundWord Junit tests

---
+++ src/main/java/wordsearch/FoundWord.java
@@ -0,0 +1,33 @@
+package wordsearch;
+import java.util.ArrayList;
+
+//*********************************************************************************************//
+//*  The FoundWord class consists of a String for the word found in the grid, as well as an   *//
+//*  ArrayList of locCoordinate objects that represent the x,y coordinates for each letter in *//
+//*  the word.                                                                                *//
+//*********************************************************************************************//
+public class FoundWord {
+
+	private String word;
+	private ArrayList<LocCoordinate> locCoordinateList;
+	
+	public FoundWord(String word, ArrayList<LocCoordinate> locCoordinateList) {
+		this.word = word;
+		this.locCoordinateList = locCoordinateList;
+	}
+	
+	@Override 
+	public String toString() {
+		// The output should not have the [] around each location coordinate.
+		// Therefore, iterate thru the ArrayList and print each one individually.
+		String retString = this.word + ": ";
+		for (int i = 0; i < locCoordinateList.size(); i++) {
+			retString += locCoordinateList.get(i).toString();
+			if (i < locCoordinateList.size() - 1) {
+				retString += ",";
+			}
+		}
+		return retString;
+	}
+}
+
