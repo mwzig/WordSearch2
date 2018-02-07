eb37fa6e850670b50a3e6662f88a48c143fd9a63
mwzig <mwziegler2014@gmail.com>
2018-02-07 11:07:15 -0500

    Add LocCoordinateTest class with tests

---
+++ src/test/java/wordsearch/LocCoordinateTest.java
@@ -0,0 +1,23 @@
+package wordsearch;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import wordsearch.LocCoordinate;
+
+public class LocCoordinateTest {
+
+	@Test
+	public void testCreateLocCoordinate() {
+		LocCoordinate locCoord = new LocCoordinate(0,5);
+		assertNotNull(locCoord);
+	}
+
+	@Test
+	public void testToString() {
+		String expectedOutput = new String("(0,5)");
+		LocCoordinate locCoord = new LocCoordinate(0,5);
+		String locCoordToString = locCoord.toString();
+		assertEquals(expectedOutput, locCoordToString);
+		
+	}
+}
